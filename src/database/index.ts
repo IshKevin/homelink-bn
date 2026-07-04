ae8b5fa0336151997b2bab2f8a8d21a0bf6897c4
IshKevin <carterk279@gmail.com>
2026-07-04 14:44:25 +0200

    add database connection setup using drizzle-orm and pg

---
+++ src/database/index.ts
@@ -0,0 +1,10 @@
+// src/database/index.ts
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import { env } from "../config/env";
+
+const pool = new Pool({
+    connectionString: env.databaseUrl
+});
+
+export const db = drizzle(pool);
